8559df577742b67ab5f162a73d973cdc8a3150de
forever97 <857426255@qq.com>
2022-06-26 06:44:55 +0000

    Site updated: 2022-06-26 06:44:53

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+
+// 本地搜索
+var localSearchTitle = document.querySelector("#local-search-title")
+localSearchTitle.innerHTML="未央の搜索魔法"
+
+// 公告
+var headLineTitle = document.querySelector(".card-announcement-animation+span")
+headLineTitle.innerHTML="未央の魔法广播"
+
+// 小鸡腿儿
+var lover = document.querySelector("[title='小鸡腿儿']")
---
+++ swiper/swiperindex.js
@@ -0,0 +1,27 @@
+let swiper = new Swiper('.blog-slider', {
+	spaceBetween: 30,
+	effect: 'fade',
+	loop: true,
+    autoplay: {
+    disableOnInteraction: true,
+	delay:3000
+    },
+	mousewheel: {
+		invert: false,
+	},
+	// autoHeight: true,
+	pagination: {
+		el: '.blog-slider__pagination',
+		clickable: true,
+	}
+});
+
+let comtainer = document.getElementById('swiper_container');
+    comtainer.onmouseenter = function () {
+        swiper.autoplay.stop();
+    };
+    comtainer.onmouseleave = function () {
+        swiper.autoplay.start();
+    };
+
+
